11bae3c23ed2270302e05bfe6fac8b48e0cf5048
anwarul <anwarulhoque032@gmail.com>
2023-02-17 16:52:01 +0600

    added services section

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import ServicesCard from "./ServicesCard";
 
 const Services = () => {
-    return (
-        <div>     
-      <h1 className='my-10 font-bold text-green-600 text-2xl text-center'>Tis is an Services section</h1>
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then(res => res.json())
+      .then(data => setServices(data));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="my-10 font-bold text-green-600 text-2xl text-center">
+        Tis is an Services section
+      </h1>
+      <div>
+        <div className="my-12 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {
+            services.map(service => 
+            <ServicesCard
+              key={service.id}
+              service= {service}
+            ></ServicesCard>)
+          }
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Services;
---
+++ src/Pages/Home/Services/ServicesCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { FaArrowRight } from 'react-icons/fa'
+// import { Link } from 'react-router-dom';
+
+const ServicesCard = ({service}) => {
+    const {  img, name, price } = service;
+    return (
+        <div className="card card-compact w-96 bg-base-100 shadow-xl p-6">
+            <figure><img src={img} alt="" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p className='text-2xl text-orange-600
+                 font-semibold'>Price: ৳ {price}</p>
+                <div className="card-actions justify-end">
+                    {/* <Link to={`/checkout/${_id}`}>
+                        <button className="btn btn-primary"><FaArrowRight></FaArrowRight></button>
+                    </Link> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServicesCard;
